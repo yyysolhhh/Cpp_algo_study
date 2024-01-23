e0ab275556d8e2447174601ae19a93d48210cfe2
yyysolhhh <solbaram37@gmail.com>
2024-01-24 03:25:41 +0900

    [0x01] 기초 코드 작성 요령 1
    4문제

--- 0x01/problem1.cpp
+++ 0x01/problem1.cpp
@@ -24,11 +24,19 @@ int func2(int arr[], int N) {
 }
 
 int func3(int N) {
-  for (int i = 0; i < N / 2; i++) {
+  for (int i = 1; i * i <= N; i++) {
     if (i * i == N)
       return 1;
   }
   return 0;
+}
+
+int func4(int N) {
+  int res;
+  for (int i = 1; i <= N; i *= 2) {
+    res = i;
+  }
+  return res;
 }
 
 int main() {
@@ -36,14 +44,26 @@ int main() {
   // cin >> N;
   // cout << func1(N);
 
-  int arr[] = {1, 52, 48};
-  int arr2[] = {50, 42};
-  int arr3[] = {4, 13, 63, 87};
-  cout << func2(arr, 3) << endl;  // 1
-  cout << func2(arr2, 2) << endl; // 0
-  cout << func2(arr3, 4) << endl; // 1
+  // // func1
+  // cout << func1(16) << endl;    // 60
+  // cout << func1(34567) << endl; // 278812814
+  // cout << func1(27639) << endl; // 178254968
 
-  cout << func3(9) << endl;         // 1
-  cout << func3(693953651) << endl; // 0
-  cout << func3(756580036) << endl; // 1
+  // // func2
+  // int arr[] = {1, 52, 48};
+  // int arr2[] = {50, 42};
+  // int arr3[] = {4, 13, 63, 87};
+  // cout << func2(arr, 3) << endl;  // 1
+  // cout << func2(arr2, 2) << endl; // 0
+  // cout << func2(arr3, 4) << endl; // 1
+
+  // // func3
+  // cout << func3(9) << endl;         // 1
+  // cout << func3(693953651) << endl; // 0
+  // cout << func3(756580036) << endl; // 1
+
+  // func4
+  cout << func4(5) << endl;        // 4
+  cout << func4(97615282) << endl; // 67108864
+  cout << func4(1024) << endl;     // 1024
 }
