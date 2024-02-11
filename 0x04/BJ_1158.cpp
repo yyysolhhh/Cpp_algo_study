788ea658d1baa1aabb9df574aa9786296358547b
yyysolhhh <solbaram37@gmail.com>
2024-02-11 16:52:34 +0900

    Rename: text -> test

--- 0x04/BJ_1158.cpp
+++ 0x04/BJ_1158.cpp
@@ -1,22 +1,23 @@
+// unsolved
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	int N, K;
-	cin >> N >> K;
+  int N, K;
+  cin >> N >> K;
 
-	list<int> L;
-	for (int i = 0; i < N; i++)
-		L.push_back(i);
-	list<int>::iterator p = L.begin();
-	cout << '<';
-	while (N--) {
-		p += K % N;
-		cout << *p << ", ";
-		L.erase(p);
-	}
-	cout << '>';
+  list<int> L;
+  for (int i = 0; i < N; i++)
+    L.push_back(i);
+  list<int>::iterator p = L.begin();
+  cout << '<';
+  while (N--) {
+    // p += K % N;
+    cout << *p << ", ";
+    L.erase(p);
+  }
+  cout << '>';
 }
--- 0x05/BJ_2493.cpp
+++ 0x05/BJ_2493.cpp
@@ -1,37 +1,35 @@
+// unsolved
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-    int N;
-    cin >> N;
+  int N;
+  cin >> N;
 
-    
-    // stack<int> tower_h;
-    // stack<int> temp;
-    // stack<int> res;
-    // for (int i = 0; i < N; i++) {
-    //     int h;
-    //     cin >> h;
-    //     tower_h.push(h);
-    // }
-    // while (!tower_h.empty()) {
-    //     int cur;
-    //     cur = tower_h.top();
-    //     tower_h.pop();
-    //     N--;
-    //     if (cur < tower_h.top()) {
-    //         res.push(N);
-    //     } else {
-    //         int temp_h;
-    //         temp_h = tower_h.top();
-    //         tower_h.pop();
-    //         temp.push(temp_h);
+  // stack<int> tower_h;
+  // stack<int> temp;
+  // stack<int> res;
+  // for (int i = 0; i < N; i++) {
+  //     int h;
+  //     cin >> h;
+  //     tower_h.push(h);
+  // }
+  // while (!tower_h.empty()) {
+  //     int cur;
+  //     cur = tower_h.top();
+  //     tower_h.pop();
+  //     N--;
+  //     if (cur < tower_h.top()) {
+  //         res.push(N);
+  //     } else {
+  //         int temp_h;
+  //         temp_h = tower_h.top();
+  //         tower_h.pop();
+  //         temp.push(temp_h);
 
-    //     }
-    // }
-
-
+  //     }
+  // }
 }
