7233adf854329a3134c81ef0094fd42cfcc1aa55
yyysolhhh <solbaram37@gmail.com>
2024-02-11 15:18:37 +0900

    Unsolved: BJ_2493

---
+++ 0x05/BJ_2493.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    
+    // stack<int> tower_h;
+    // stack<int> temp;
+    // stack<int> res;
+    // for (int i = 0; i < N; i++) {
+    //     int h;
+    //     cin >> h;
+    //     tower_h.push(h);
+    // }
+    // while (!tower_h.empty()) {
+    //     int cur;
+    //     cur = tower_h.top();
+    //     tower_h.pop();
+    //     N--;
+    //     if (cur < tower_h.top()) {
+    //         res.push(N);
+    //     } else {
+    //         int temp_h;
+    //         temp_h = tower_h.top();
+    //         tower_h.pop();
+    //         temp.push(temp_h);
+
+    //     }
+    // }
+
+
+}
