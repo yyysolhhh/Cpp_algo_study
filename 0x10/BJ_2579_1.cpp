3161014ee4de6f75469379944116dc89a0178247
yyysolhhh <solbaram37@gmail.com>
2024-07-17 21:28:26 +0900

    [0x10] dp.md, BJ_2579

---
+++ 0x10/BJ_2579_1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int stair;
+int point[301];
+int dp[301][3];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> stair;
+  for (int i = 1; i <= stair; i++) {
+    cin >> point[i];
+  }
+  if (stair == 1) {
+    cout << point[1];
+    return 0;
+  }
+  dp[1][1] = point[1];
+  // dp[1][2] = 0;
+  // dp[2][1] = point[2];
+  // dp[2][2] = point[1] + point[2];
+  for (int i = 2; i <= stair; i++) {
+    dp[i][1] = point[i] + max(dp[i - 2][1], dp[i - 2][2]);
+    dp[i][2] = point[i] + dp[i - 1][1];
+  }
+  cout << max(dp[stair][1], dp[stair][2]);
+  return 0;
+}
