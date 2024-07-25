25f07f358da7c75be35d530985d320ccfb046dcf
yyysolhhh <solbaram37@gmail.com>
2024-07-25 16:56:58 +0900

    Solve: BJ_1021

--- 0x07/BJ_1021.cpp
+++ 0x07/BJ_1021.cpp
@@ -42,32 +42,32 @@ int main(void) {
 }
 
 // 2
-// int main(void) {
-//   ios::sync_with_stdio(0);
-//   cin.tie(0);
-//   int N, M;
-//   cin >> N >> M;
-//   deque<int> dq;
-//   for (int i = 1; i <= N; i++) {
-//     dq.push_back(i);
-//   }
-//   int ans = 0;
-//   while (M--) {
-//     int t;
-//     cin >> t;
-//     int idx = find(dq.begin(), dq.end(), t) - dq.begin();
-//     while (dq.front() != t) {
-//       if (idx < dq.size() - idx) {
-//         dq.push_back(dq.front());
-//         dq.pop_front();
-//       } else {
-//         dq.push_front(dq.back());
-//         dq.pop_back();
-//       }
-//       ans++;
-//     }
-//     dq.pop_front();
-//   }
-//   cout << ans;
-//   return 0;
-// }
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N, M;
+  cin >> N >> M;
+  deque<int> dq;
+  for (int i = 1; i <= N; i++) {
+    dq.push_back(i);
+  }
+  int ans = 0;
+  while (M--) {
+    int t;
+    cin >> t;
+    int idx = find(dq.begin(), dq.end(), t) - dq.begin();
+    while (dq.front() != t) {
+      if (idx < int(dq.size()) - idx) {
+        dq.push_back(dq.front());
+        dq.pop_front();
+      } else {
+        dq.push_front(dq.back());
+        dq.pop_back();
+      }
+      ans++;
+    }
+    dq.pop_front();
+  }
+  cout << ans;
+  return 0;
+}
